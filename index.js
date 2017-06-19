74690e3c66e648ac8d7113edc1ee0bb9cebd3fb0
Steve <stevenjchang@gmail.com>
2017-06-19 15:35:41 -0700

    Implement Playround

--- index.js
+++ index.js
@@ -24,16 +24,34 @@ Board.prototype = {
 }
 
 var PlayRound = function() {
+  var roundOver = false;
+  var currentPlayer = 1;
   var board = new Board();
 
   prompt.get(['player', 'x', 'y'], function (err, result) {
-    console.log('Command-line input received:');
+    console.log('player ones turn');
     console.log('  players turn: ' + result.player);
     console.log('  x coordinate: ' + result.x);
     console.log('  y coordinate: ' + result.y);
   });
 
-  
+  while (roundOver === false) {
+    if (currentPlayer === 1) {
+      console.log('Player 1, enter your move')
+      prompt.get(['x', 'y'], function(err, result) {
+        console.log(' player1 placed X on: ' + result.x + ', ' + result.y)
+      });
+      board.insert(1, result.x, result.y)
+      currentPlayer = 2;
+    } else if (currentPlayer === 2){
+      console.log('Player 2, enter your move')
+      prompt.get(['x', 'y'], function(err, result) {
+        console.log(' player2 placed X on: ' + result.x + ', ' + result.y)
+      });
+      board.insert(2, result.x, result.y)
+      currentPlayer = 1;
+    }
+  }
 
 }
 
