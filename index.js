5a4958c59abcaa556532d5b6fb9d39d31c00b371
Steve <stevenjchang@gmail.com>
2017-06-19 15:26:58 -0700

    import npm prompt, execute basic prompt.get

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-//make a board (3 x 3 array)
+var prompt = require('prompt');
 
-//insert(x,y) function, will insert X or O at given coordinates
-
-// [
-//   [0,0,0],
-//   [0,0,0],
-//   [0,0,0]
-// ]
-
-//check for victory 
-  //if all of current x is the same value OR all of current y is the same value
-    //return current player wins
-
-const Board = () => {
+var Board = function() {
   var storage = [[0,0,0],[0,0,0],[0,0,0]];
   return storage;
 }
 
-Board.prototype.insert = (player, x, y) => {
-  this.storage[x][y] = player;
-
-  //check if player has won
-  if (this.storage[x][0] === player, 
-      this.storage[x][1] === player, 
-      this.storage[x][2] === player ||
-      this.storage[0][y] === player,
-      this.storage[1][y] === player,
-      this.storage[2][y] === player) {
-        return player + " has Won!";
+Board.prototype = {
+  insert: function(player, x, y) {
+    this.storage[x][y] = player;
+    //check if player has won
+    if (this.storage[x][0] === player, 
+        this.storage[x][1] === player, 
+        this.storage[x][2] === player ||
+        this.storage[0][y] === player,
+        this.storage[1][y] === player,
+        this.storage[2][y] === player) {
+          return player + " has Won!";
+    }
+  },
+  revert: function() {
+    console.log('reverted');
   }
 }
 
+var PlayRound = function() {
+  var board = new Board();
+
+  prompt.get(['player', 'x', 'y'], function (err, result) {
+    console.log('Command-line input received:');
+    console.log('  players turn: ' + result.player);
+    console.log('  x coordinate: ' + result.x);
+    console.log('  y coordinate: ' + result.y);
+  });
+
+  
+
+}
+
+PlayRound();
 
