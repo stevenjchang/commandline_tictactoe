5e75a1646994cd03d1b6b7c2c1f2497014092109
Steve <stevenjchang@gmail.com>
2017-06-19 15:05:34 -0700

    Board.prototype.insert function

--- index.js
+++ index.js
@@ -11,3 +11,23 @@
 //check for victory 
   //if all of current x is the same value OR all of current y is the same value
     //return current player wins
+
+const Board = () => {
+  var storage = [[0,0,0],[0,0,0],[0,0,0]];
+  return storage;
+}
+
+Board.prototype.insert = (player, x, y) => {
+  this.storage[x][y] = player;
+
+  //check if player has won
+  if (this.storage[x][0] === player, 
+      this.storage[x][1] === player, 
+      this.storage[x][2] === player ||
+      this.storage[0][y] === player,
+      this.storage[1][y] === player,
+      this.storage[2][y] === player) {
+        return player + " has Won!";
+  }
+}
+
