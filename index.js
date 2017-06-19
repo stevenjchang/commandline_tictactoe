e2d078f423b83e6c4e0d5507b5cddb4ea5afabc9
Steve <stevenjchang@gmail.com>
2017-06-19 14:52:58 -0700

    basic pseudocode

---
+++ index.js
@@ -0,0 +1,13 @@
+//make a board (3 x 3 array)
+
+//insert(x,y) function, will insert X or O at given coordinates
+
+// [
+//   [0,0,0],
+//   [0,0,0],
+//   [0,0,0]
+// ]
+
+//check for victory 
+  //if all of current x is the same value OR all of current y is the same value
+    //return current player wins
